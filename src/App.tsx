b7253ede41549ab3247dec388e79075cc6f364b6
muditxdev <muditjain1934@gmail.com>
2025-07-08 22:17:43 +0530

    feat: fix some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import Dashboard from './components/Dashboard/Dashboard';
 
