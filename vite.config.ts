d0da635601a655bed4c8a6415100fdb7f05b4f7e
muditxdev <muditjain1934@gmail.com>
2025-07-08 22:06:21 +0530

    feat: updated readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const repoName = "sales-dashboard";
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
